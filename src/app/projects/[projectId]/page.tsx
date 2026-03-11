a0e77372956f9736e462f993df1383dcc5438be5
Parth-S-Mahale <parthmahale05@gmail.com>
2026-03-12 00:11:31 +0530

    19: bug-fixes

--- src/app/projects/[projectId]/page.tsx
+++ src/app/projects/[projectId]/page.tsx
@@ -4,31 +4,38 @@ import { getQueryClient, trpc } from "@/trpc/server";
 
 import { ProjectView } from "@/modules/projects/ui/views/project-view";
 import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 
 interface Props {
-    params: Promise<{
-        projectId: string
-    }>
+  params: Promise<{
+    projectId: string;
+  }>;
 }
 
-const page = async ({params}: Props) => {
-  const {projectId} = await params;
+const page = async ({ params }: Props) => {
+  const { projectId } = await params;
 
   const queryClient = getQueryClient();
 
-  void queryClient.prefetchQuery(trpc.messages.getMany.queryOptions({
-    projectId: projectId,
-  }));
+  void queryClient.prefetchQuery(
+    trpc.messages.getMany.queryOptions({
+      projectId: projectId,
+    }),
+  );
 
-  void queryClient.prefetchQuery(trpc.projects.getOne.queryOptions({
-    id: projectId,
-  }));
+  void queryClient.prefetchQuery(
+    trpc.projects.getOne.queryOptions({
+      id: projectId,
+    }),
+  );
 
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
-      <Suspense fallback={<p>Loading...</p>}>
-        <ProjectView projectId={projectId} />
-      </Suspense>
+      <ErrorBoundary fallback={<p>Error!</p>}>
+        <Suspense fallback={<p>Loading...</p>}>
+          <ProjectView projectId={projectId} />
+        </Suspense>
+      </ErrorBoundary>
     </HydrationBoundary>
   );
 };
